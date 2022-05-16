74af386d37d8f8cff5eb7ffe63874bd2f42084d0
chae214 <ssohappy09@gmail.com>
2022-05-16 16:37:00 +0900

    ui ver2.

---
+++ app/src/main/java/com/example/getthepicture_2/FindmailActivity.java
@@ -0,0 +1,13 @@
+package com.example.getthepicture_2;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FindmailActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_findmail);
+    }
+}
--- app/src/main/java/com/example/getthepicture_2/LoginActivity.java
+++ app/src/main/java/com/example/getthepicture_2/LoginActivity.java
@@ -13,11 +13,19 @@ public class LoginActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button forgotbtn=(Button) findViewById(R.id.login_forgot);
-        forgotbtn.setOnClickListener(new View.OnClickListener(){
+        Button forgotid=(Button) findViewById(R.id.email_forgot);
+        Button forgotpw=(Button) findViewById(R.id.pw_forgot);
+        forgotpw.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(LoginActivity.this,FindpwActivity.class);
+                startActivity((intent));
+            }
+        });
+        forgotid.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,FindmailActivity.class);
                 startActivity((intent));
             }
         });
