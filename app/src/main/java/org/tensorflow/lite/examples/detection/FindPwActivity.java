65d41010c333308c54255b96f1bd979d4aa76637
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-05-27 20:54:29 +0900

    음성 검색으로 이미지 목록 삭제기능 추가, 이미지 목록 읽기 기능 추가 로그인 기능 추가
    작성자: 송승민
    작성일: 2022-05-27

--- app/src/main/java/org/tensorflow/lite/examples/detection/FindPwActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/FindPwActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FindpwActivity extends AppCompatActivity{
+public class FindPwActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
